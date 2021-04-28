f6b65d8a679e6d4c0398d6e92e994593d7945708
itamar659 <itamar.ayalon@gmail.com>
2021-04-28 19:06:43 +0300

    Created Symbol enum and Player class

---
+++ B21 Ex02 YanivAvrahami 302374921 ItamarAyalon 206024796/B21_Ex02/Player.cs
@@ -0,0 +1,33 @@
+﻿namespace B21_Ex02
+{
+    public class Player
+    {
+        private eSymbol m_Symbol;
+
+        private int m_Score;
+
+        public int Score
+        {
+            get
+            {
+                return m_Score;
+            }
+            set
+            {
+                m_Score = value;
+            }
+        }
+
+        public eSymbol Symbol
+        {
+            get
+            {
+                return m_Symbol;
+            }
+            set
+            {
+                m_Symbol = value;
+            }
+        }
+    }
+}
