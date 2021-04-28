606e71d6a49c3bd1dfc70e655003728d22d27d47
itamar659 <itamar.ayalon@gmail.com>
2021-04-28 19:40:02 +0300

    Created Board class, and a private nested BoardItem struct

--- B21 Ex02 YanivAvrahami 302374921 ItamarAyalon 206024796/B21_Ex02/Data Structures/Board.cs
+++ B21 Ex02 YanivAvrahami 302374921 ItamarAyalon 206024796/B21_Ex02/Data Structures/Board.cs
@@ -1,10 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace B21_Ex02
+﻿namespace B21_Ex02
 {
-    class Board
+    public class Board
     {
+        private struct BoardItem
+        {
+            private bool isOccupied;
+            private eSymbol m_Symbol;
+
+            public eSymbol Symbol
+            {
+                get
+                {
+                    return m_Symbol;
+                }
+                set
+                {
+                    m_Symbol = value;
+                }
+            }
+
+            public bool IsOccupied
+            {
+                get
+                {
+                    return isOccupied;
+                }
+                set
+                {
+                    isOccupied = true;
+                }
+            }
+
+            public BoardItem(eSymbol i_Symbol)
+            {
+                isOccupied = true;
+                m_Symbol = i_Symbol;
+            }
+        }
+
+        private readonly BoardItem[,] m_Board;
+
+        public int Size
+        {
+            get
+            {
+                return m_Board.Length;
+            }
+        }
+
+        public Board(int i_Rows, int i_Columns)
+        {
+            m_Board = new BoardItem[i_Rows,i_Columns];
+        }
+
+        public void SetItem(eSymbol i_Symbol, int i_Row, int i_Column)
+        {
+            m_Board[i_Row, i_Column] = new BoardItem(i_Symbol);
+        }
+
+        public eSymbol GetItem(int i_Row, int i_Column)
+        {
+            return m_Board[i_Row, i_Column].Symbol;
+        }
+
+        public void Delete(int i_Row, int i_Column)
+        {
+            m_Board[i_Row, i_Column].IsOccupied = false;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < m_Board.Length; i++)
+            {
+                for (int j = 0; j < m_Board.Length; j++)
+                {
+                    Delete(i, j);
+                }
+            }
+        }
+
+        public bool IsOccupied(int i_Row, int i_Column)
+        {
+            return m_Board[i_Row, i_Column].IsOccupied;
+        }
     }
 }
--- B21 Ex02 YanivAvrahami 302374921 ItamarAyalon 206024796/B21_Ex02/Data Structures/BoardViewer.cs
+++ B21 Ex02 YanivAvrahami 302374921 ItamarAyalon 206024796/B21_Ex02/Data Structures/BoardViewer.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace B21_Ex02
 {
-    class BoardViewer
+    public class BoardViewer
     {
+        public static void DisplayOnConsole(Board i_Board)
+        {
+            StringBuilder BoardStrBuldr = new StringBuilder();
+
+
+        }
     }
 }
