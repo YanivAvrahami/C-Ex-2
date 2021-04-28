4e84c84bbc0b1496b606bb22e5d498e957a1a2cd
Yaniv Avrahami <avrahamiyaniv@gmail.com>
2021-04-28 18:44:28 +0300

    Init

---
+++ B21 Ex02 Yaniv 302374921 Itamar 206024796/B21_Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B21_Ex02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("HELLO ITAMAR!");
+        }
+    }
+}
